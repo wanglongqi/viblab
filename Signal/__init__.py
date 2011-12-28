7c8a6388369e6903c7c201527f1da00d2bfe6bad
wlongqi@gmail.com <wlongqi@gmail.com@d8655957-5e2a-033f-b728-24be456d7827>
2011-12-28 01:31:21 +0000

    添加__init__代码用来模块化

---
+++ Signal/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+'''Signal contains modules about vibration signal analysis.
+
+:see: http://daye.gofreeserve.com
+:see: http://code.google.com/p/viblab/
+:see: http://code.google.com/p/abaqus-helper/
+
+Created on 2011-12-28
+
+@author: WLQ
+'''
+import spec
+from vibstat import crest,dB,dBwin,fastmoving,moving,peak,postdict,rms,vdv
+from weighting import Weight
+import windows as win
+
---
+++ __init__.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+'''VibLab contains modules about vibration signal analysis.
+
+This package is basically composed of two major parts:
+
+ * handling I/O from some not widely used filetype to Python, e.g. DASP I/O file.
+ * vibration signal analysis in time/frequency domain.
+
+:see: http://daye.gofreeserve.com
+:see: http://code.google.com/p/viblab/
+:see: http://code.google.com/p/abaqus-helper/
+
+Created on 2011-12-28
+
+@author: WLQ
+'''
+
+from DASP import *
+from Signal import *
