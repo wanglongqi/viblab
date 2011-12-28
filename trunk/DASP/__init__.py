8a14a33c5850d2bf33159c7c3350b9720c86b162
wlongqi@gmail.com <wlongqi@gmail.com@d8655957-5e2a-033f-b728-24be456d7827>
2011-12-28 01:31:21 +0000

    添加__init__代码用来模块化

---
+++ trunk/DASP/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+'''DASPIO contains modules about I/O from some not widely used filetype to Python.
+
+:see: http://daye.gofreeserve.com
+:see: http://code.google.com/p/viblab/
+:see: http://code.google.com/p/abaqus-helper/
+
+Created on 2011-12-28
+
+@author: WLQ
+'''
+
+from ary2dsp import ary2dsp,ary2sts
+from dsp2ary import dsp2ary , dsp2mat
+from dsp2hdf import dsp2hdf
+from hdf2dsp import hdf2dsp
