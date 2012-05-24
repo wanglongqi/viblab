dc4a5dfd260b1ab3419082740f3c6501c9f2bc91
wlongqi@gmail.com <wlongqi@gmail.com@d8655957-5e2a-033f-b728-24be456d7827>
2012-05-24 05:14:39 +0000

    改为可以带多参数

--- trunk/Others/ExC.py
+++ trunk/Others/ExC.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 '''
-Created on 2011-12-22
+Created on 2012-5-24
 
 @author: WLQ
 贵金属价格美元至人民币兑换程序，使用了Sina提供的汇率
@@ -11,15 +11,13 @@ import os
 
 exchange=float(urllib2.urlopen("http://hq.sinajs.cn/list=USDCNY").read().split(',')[7])
 if len(sys.argv)>1:
-    price=sys.argv[1]
-    if price.find('$')==-1:
-        print price,'CNY/g -> %.2f USD/oz'%(float(price)*31.1035/exchange)
-    else:
-        price=price.replace('$','')
-        print price,'USD/oz -> %.2f CNY/g'%(float(price)/31.1035*exchange)
+    for price in sys.argv[1:]:
+        if price.find('$')==-1:
+            print price,'CNY/g -> %.2f USD/oz'%(float(price)*31.1035/exchange)
+        else:
+            price=price.replace('$','')
+            print price,'USD/oz -> %.2f CNY/g'%(float(price)/31.1035*exchange)
 else:
     print 'Usage:',os.path.relpath(sys.argv[0]),'''PRICE
     PRICE: price in CNY/gram or USD/oz(with $), eg 20.1 or 1530.35$'''
         
-
-
